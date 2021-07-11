307dcd3235e31d5896b6b7e1b62860729a33e56c
Brandon Bloom <brandon@brandonbloom.name>
2021-07-11 15:14:49 -0700

    delete exoweb

--- exoweb/src/lib/routing.ts
+++
@@ -1,7 +0,0 @@
-export interface Route<TProps> {
-  pattern: string;
-  load: () => Promise<TProps>;
-  render: (props: TProps) => React.ReactNode;
-}
-
-export const makeRoute = <TProps>(route: Route<TProps>): Route<TProps> => route;
--- exoweb/src/routes.ts
+++
@@ -1,7 +0,0 @@
-import { Route } from './lib/routing';
-import { route as homeRoute } from './pages/home';
-import { route as computeRoute } from './pages/compute';
-
-const routes: Route<any>[] = [homeRoute, computeRoute];
-
-export default routes;
