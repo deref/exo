29aca9f1a2379bad11bc4e4df396b33ad46563cc
Brandon Bloom <brandon@brandonbloom.name>
2021-07-15 11:11:39 -0700

    project josh

--- kernel/api/project.go
+++ kernel/api/project.go
@@ -1,10 +1,11 @@
+// Generated file. DO NOT EDIT.
+
 package api
 
 import (
 	"context"
 	"net/http"
 
-	"github.com/deref/exo/config"
 	josh "github.com/deref/exo/josh/server"
 )
 
@@ -15,10 +16,8 @@ type Project interface {
 	Apply(context.Context, *ApplyInput) (*ApplyOutput, error)
 	// Refreshes all components.
 	Refresh(context.Context, *RefreshInput) (*RefreshOutput, error)
-
 	// Resolves a reference in to an ID.
 	Resolve(context.Context, *ResolveInput) (*ResolveOutput, error)
-
 	// Returns component descriptions.
 	DescribeComponents(context.Context, *DescribeComponentsInput) (*DescribeComponentsOutput, error)
 	// Creates a component and triggers an initialize lifecycle event.
@@ -27,35 +26,35 @@ type Project interface {
 	UpdateComponent(context.Context, *UpdateComponentInput) (*UpdateComponentOutput, error)
 	// Triggers a refresh lifecycle event to update the component's state.
 	RefreshComponent(context.Context, *RefreshComponentInput) (*RefreshComponentOutput, error)
-	// Marks a component as disposed and triggers the dispose lifecycle event.
-	// After being disposed, the component record will be deleted asynchronously.
+	// Marks a component as disposed and triggers the dispose lifecycle event. After being disposed, the component record will be deleted asynchronously.
 	DisposeComponent(context.Context, *DisposeComponentInput) (*DisposeComponentOutput, error)
 	// Disposes a component and then awaits the record to be deleted synchronously.
 	DeleteComponent(context.Context, *DeleteComponentInput) (*DeleteComponentOutput, error)
-
 	DescribeLogs(context.Context, *DescribeLogsInput) (*DescribeLogsOutput, error)
 	GetEvents(context.Context, *GetEventsInput) (*GetEventsOutput, error)
-
 	Start(context.Context, *StartInput) (*StartOutput, error)
 	Stop(context.Context, *StopInput) (*StopOutput, error)
-
-	// TODO: Move these to a plugin or similar.
 	DescribeProcesses(context.Context, *DescribeProcessesInput) (*DescribeProcessesOutput, error)
 }
 
-type DeleteInput struct{}
-
-type DeleteOutput struct{}
-
-type ApplyInput struct {
-	Config config.Config `json:"config"`
+type DeleteInput struct {
 }
 
-type ApplyOutput struct{}
+type DeleteOutput struct {
+}
 
-type RefreshInput struct{}
+type ApplyInput struct {
+	Config string `json:"config"`
+}
 
-type RefreshOutput struct{}
+type ApplyOutput struct {
+}
+
+type RefreshInput struct {
+}
+
+type RefreshOutput struct {
+}
 
 type ResolveInput struct {
 	Refs []string `json:"refs"`
@@ -65,21 +64,11 @@ type ResolveOutput struct {
 	IDs []*string `json:"ids"`
 }
 
-type DescribeComponentsInput struct{}
+type DescribeComponentsInput struct {
+}
 
 type DescribeComponentsOutput struct {
 	Components []ComponentDescription `json:"components"`
-}
-
-type ComponentDescription struct {
-	ID          string  `json:"id"`
-	Name        string  `json:"name"`
-	Type        string  `json:"type"`
-	Spec        string  `json:"spec"`
-	State       string  `json:"state"`
-	Created     string  `json:"created"`
-	Initialized *string `json:"initialized"`
-	Disposed    *string `json:"disposed"`
 }
 
 type CreateComponentInput struct {
@@ -97,25 +86,29 @@ type UpdateComponentInput struct {
 	Spec string `json:"spec"`
 }
 
-type UpdateComponentOutput struct{}
-
-type DisposeComponentInput struct {
-	Ref string `json:"ref"`
+type UpdateComponentOutput struct {
 }
 
 type RefreshComponentInput struct {
 	Ref string `json:"ref"`
 }
 
-type RefreshComponentOutput struct{}
+type RefreshComponentOutput struct {
+}
 
-type DisposeComponentOutput struct{}
+type DisposeComponentInput struct {
+	Ref string `json:"ref"`
+}
+
+type DisposeComponentOutput struct {
+}
 
 type DeleteComponentInput struct {
 	Ref string `json:"ref"`
 }
 
-type DeleteComponentOutput struct{}
+type DeleteComponentOutput struct {
+}
 
 type DescribeLogsInput struct {
 	Refs []string `json:"refs"`
@@ -123,11 +116,6 @@ type DescribeLogsInput struct {
 
 type DescribeLogsOutput struct {
 	Logs []LogDescription `json:"logs"`
-}
-
-type LogDescription struct {
-	Name        string  `json:"name"`
-	LastEventAt *string `json:"lastEventAt"`
 }
 
 type GetEventsInput struct {
@@ -140,53 +128,76 @@ type GetEventsOutput struct {
 	Events []Event `json:"events"`
 }
 
-type Event struct {
-	Log       string `json:"log"`
-	Sid       string `json:"sid"`
-	Timestamp string `json:"timestamp"`
-	Message   string `json:"message"`
-}
-
 type StartInput struct {
 	Ref string `json:"ref"`
 }
 
-type StartOutput struct{}
+type StartOutput struct {
+}
 
 type StopInput struct {
 	Ref string `json:"ref"`
 }
 
-type StopOutput struct{}
+type StopOutput struct {
+}
 
-type DescribeProcessesInput struct{}
+type DescribeProcessesInput struct {
+}
 
 type DescribeProcessesOutput struct {
 	Processes []ProcessDescription `json:"processes"`
+}
+
+func NewProjectMux(prefix string, iface Project) *http.ServeMux {
+	b := josh.NewMuxBuilder(prefix)
+	BuildProjectMux(b, iface)
+	return b.Mux()
+}
+
+func BuildProjectMux(b *josh.MuxBuilder, iface Project) {
+	b.AddMethod("delete", iface.Delete)
+	b.AddMethod("apply", iface.Apply)
+	b.AddMethod("refresh", iface.Refresh)
+	b.AddMethod("resolve", iface.Resolve)
+	b.AddMethod("describe-components", iface.DescribeComponents)
+	b.AddMethod("create-component", iface.CreateComponent)
+	b.AddMethod("update-component", iface.UpdateComponent)
+	b.AddMethod("refresh-component", iface.RefreshComponent)
+	b.AddMethod("dispose-component", iface.DisposeComponent)
+	b.AddMethod("delete-component", iface.DeleteComponent)
+	b.AddMethod("describe-logs", iface.DescribeLogs)
+	b.AddMethod("get-events", iface.GetEvents)
+	b.AddMethod("start", iface.Start)
+	b.AddMethod("stop", iface.Stop)
+	b.AddMethod("describe-processes", iface.DescribeProcesses)
+}
+
+type ComponentDescription struct {
+	ID          string  `json:"id"`
+	Name        string  `json:"name"`
+	Type        string  `json:"type"`
+	Spec        string  `json:"spec"`
+	State       string  `json:"state"`
+	Created     string  `json:"created"`
+	Initialized *string `json:"initialized"`
+	Disposed    *string `json:"disposed"`
+}
+
+type LogDescription struct {
+	Name        string  `json:"name"`
+	LastEventAt *string `json:"lastEventAt"`
+}
+
+type Event struct {
+	Log       string `json:"log"`
+	SID       string `json:"sid"`
+	Timestamp string `json:"timestamp"`
+	Message   string `json:"message"`
 }
 
 type ProcessDescription struct {
 	ID      string `json:"id"`
 	Name    string `json:"name"`
 	Running bool   `json:"running"`
-}
-
-func NewProjectMux(prefix string, project Project) *http.ServeMux {
-	b := josh.NewMuxBuilder(prefix)
-	b.AddMethod("delete", project.Delete)
-	b.AddMethod("apply", project.Apply)
-	b.AddMethod("refresh", project.Refresh)
-	b.AddMethod("resolve", project.Resolve)
-	b.AddMethod("describe-components", project.DescribeComponents)
-	b.AddMethod("create-component", project.CreateComponent)
-	b.AddMethod("update-component", project.UpdateComponent)
-	b.AddMethod("refresh-component", project.RefreshComponent)
-	b.AddMethod("dispose-component", project.DisposeComponent)
-	b.AddMethod("delete-component", project.DeleteComponent)
-	b.AddMethod("describe-logs", project.DescribeLogs)
-	b.AddMethod("get-events", project.GetEvents)
-	b.AddMethod("start", project.Start)
-	b.AddMethod("stop", project.Stop)
-	b.AddMethod("describe-processes", project.DescribeProcesses)
-	return b.Mux()
 }
---
+++ kernel/api/project.josh.hcl
@@ -0,0 +1,121 @@
+interface "project" {
+  method "delete" {
+    doc = "Deletes all of the components in the project, then deletes the project itself."
+  }
+  
+  method "apply" {
+    doc = "Performs creates, updates, refreshes, disposes, as needed."
+
+    input "config" "string" {}
+  }
+  
+  method "refresh" {
+    doc = "Refreshes all components."
+  }
+
+  method "resolve" {
+    doc = "Resolves a reference in to an ID."
+
+    input "refs" "[]string" {}
+
+    output "ids" "[]*string" {}
+  }
+
+  method "describe-components" {
+	  doc = "Returns component descriptions."
+
+    output "components" "[]ComponentDescription" {}
+  }
+  
+  method "create-component" {
+    doc = "Creates a component and triggers an initialize lifecycle event."
+
+	  input "name" "string" {}
+	  input "type" "string" {}
+	  input "spec" "string" {}
+
+    output "id" "string" {}
+  }
+  
+  method "update-component" {
+	  doc = "Replaces the spec on a component and triggers an update lifecycle event."
+
+    input "ref" "string" {}
+    input "spec" "string" {}
+  }
+  
+  method "refresh-component" {
+	  doc = "Triggers a refresh lifecycle event to update the component's state."
+      
+    input "ref" "string" {}
+  }
+  
+  method "dispose-component" {
+    # TODO: Line breaks in doc strings.
+    doc = "Marks a component as disposed and triggers the dispose lifecycle event. After being disposed, the component record will be deleted asynchronously."
+      
+    input "ref" "string" {}
+  }
+  
+  method "delete-component" {
+	  doc = "Disposes a component and then awaits the record to be deleted synchronously."
+     
+    input "ref" "string" {}
+  }
+
+  method "describe-logs" {
+    input "refs" "[]string" {}
+    
+    output "logs" "[]LogDescription" {}
+  }
+  
+  method "get-events" {
+	  input "logs" "[]string" {}
+	  input "before" "string" {}
+	  input "after" "string" {}
+    
+    output "events" "[]Event" {}
+  }
+
+  method "start" {
+    input "ref" "string" {}
+  }
+  
+  method "stop" {
+    input "ref" "string" {}
+  }
+
+	// TODO: Move these to a plugin or similar.
+  method "describe-processes" {
+    output "processes" "[]ProcessDescription" {}
+  }
+}
+
+struct "component-description" {
+	field "id" "string" {}
+	field "name" "string" {}
+	field "type" "string" {}
+	field "spec" "string" {}
+	field "state" "string" {}
+	field "created" "string" {}
+	field "initialized" "*string" {}
+	field "disposed" "*string" {}
+}
+
+struct "log-description" {
+  field "name" "string" {}
+  field "last-event-at" "*string" {}
+}
+
+struct "event" {
+	field "log" "string" {}
+	field "sid" "string" {}
+	field "timestamp" "string" {}
+	field "message" "string" {}
+}
+
+struct "process-description" {
+	field "id" "string" {}
+	field "name" "string" {}
+	field "running" "bool" {}
+}
