1e9d669985e9e1e2d619624eb3e766f442892625
Brandon Bloom <brandon@brandonbloom.name>
2021-07-15 08:46:58 -0700

    logd socket

--- cmd/exop/main.go
+++ cmd/exop/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"net"
 	"net/http"
+	"path/filepath"
 
 	"github.com/deref/exo/components/log"
 	josh "github.com/deref/exo/josh/client"
@@ -13,9 +15,17 @@ import (
 
 func main() {
 	ctx := server.NewContext(context.Background())
+	varDir := "./var" // XXX parameterize and pass this around.
 	ctx = log.ContextWithLogCollector(ctx, logcol.NewLogCollector(&josh.Client{
-		HTTP: http.DefaultClient,
-		URL:  "http://localhost:3001/",
+		HTTP: &http.Client{
+			Transport: &http.Transport{
+				DialContext: func(ctx context.Context, network string, addr string) (net.Conn, error) {
+					dialer := net.Dialer{}
+					return dialer.DialContext(ctx, "unix", filepath.Join(varDir, "logcol.sock"))
+				},
+			},
+		},
+		URL: "http://unix/",
 	}))
 	pier.Main(server.NewHandler(ctx))
 }
--- cmd/logd/main.go
+++ cmd/logd/main.go
@@ -1,22 +1,28 @@
-// Separate logd service for testing in isolation. Unused for production deployments.
+// Separate logd service for testing in isolation. Unused for production
+// deployments.  By default, binds a unix domain socket for easy discovery from
+// exop.  If PORT environment variable is set, listens there instead for easy
+// testing via curl or httpie.
 
 package main
 
 import (
 	"context"
+	"fmt"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
 
+	"github.com/deref/exo/cmdutil"
 	"github.com/deref/exo/logcol/api"
 	"github.com/deref/exo/logcol/server"
 )
 
 func main() {
 	ctx := context.Background()
-	port := os.Getenv("PORT")
 	collector := server.NewLogCollector()
 	collector.Start(ctx)
+
 	go func() {
 		c := make(chan os.Signal, 1)
 		signal.Notify(c, os.Interrupt)
@@ -24,5 +30,27 @@ func main() {
 		collector.Stop(ctx)
 		os.Exit(0)
 	}()
-	http.ListenAndServe(":"+port, api.NewLogCollectorMux("/", collector))
+
+	port := os.Getenv("PORT")
+	var network, addr string
+	if port == "" {
+		network = "unix"
+		addr = "./var/logcol.sock"
+		_ = os.Remove(addr)
+	} else {
+		network = "tcp"
+		addr = ":" + port
+	}
+	listener, err := net.Listen(network, addr)
+	if err != nil {
+		cmdutil.Fatalf("error listening: %v", err)
+	}
+
+	fmt.Println("listening at", addr)
+
+	handler := api.NewLogCollectorMux("/", collector)
+	server := http.Server{
+		Handler: handler,
+	}
+	server.Serve(listener)
 }
