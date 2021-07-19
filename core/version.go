9371df76fcfa9ef14f04c0b43f6b3006f42b4928
Andrew Meredith <andymeredith@gmail.com>
2021-07-19 15:22:53 -0600

    Add self-update command

--- core/version.go
+++ core/version.go
@@ -2,5 +2,10 @@ package core
 
 import _ "embed"
 
+const (
+	CheckVersionEndpoint = "https://download-page.deref.workers.dev/version.txt"
+	UpdateScriptEndpoint = "https://download-page.deref.workers.dev/install.sh"
+)
+
 //go:embed VERSION
 var Version string
