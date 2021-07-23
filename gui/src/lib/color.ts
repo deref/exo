5cfad5fc96bd8f80a45a949ae14e7a1494a62a67
John <51100181+jwmza@users.noreply.github.com>
2021-07-23 08:13:54 -0400

    Add comments

--- gui/src/lib/color.ts
+++ gui/src/lib/color.ts
@@ -1,3 +1,5 @@
+// This is a palette of colors intended to be different enough from one another
+// that distinct processes look distinct in the log viewer.
 export const palette = [
   [
   '#0071f9',
@@ -31,17 +33,18 @@ export const palette = [
   ],
 ]
 
+// This computes an integer hash which can be used to select a palette.
 export const hashString = (s: string) => s.split('').map(c => c.charCodeAt(0)).reduce((a,c) => a + c)
 
+// This selects a color palette based on the hash of a string.
 export const hashPalette = (s: string) => palette[hashString(s) % palette.length]
 
+// This computes a HTML style attribute string for colored logs.
 export const logStyleFromHash = (s: string) => {
-
+  // Format a single color variable.
   const f = (varName, paletteIndex) => { 
-    
     return `--${varName}:${hashPalette(s)[paletteIndex]};`
   }
-
+  // Combine styles into one string.
   return f("log-color",0) + f("log-border-color",1) + f("log-bg-color",2)
-
 }
