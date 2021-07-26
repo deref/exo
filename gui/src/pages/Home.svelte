f55a94663cc9fbb9b2c1786406406e3cfd114c18
John <51100181+jwmza@users.noreply.github.com>
2021-07-26 19:19:15 -0400

    Style and add click routes to workspace list on home page

--- gui/src/pages/Home.svelte
+++ gui/src/pages/Home.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Layout from '../components/Layout.svelte';
   import Code from '../components/Code.svelte';
+  import * as router from 'svelte-spa-router';
   import { api } from '../lib/api';
 
   const workspaces = api.kernel.describeWorkspaces();
@@ -10,13 +11,22 @@
   <section>
     Use <Code>exo gui</Code> in your terminal.
     <!-- TODO: Use this data below: !-->
-    <div style="display: none">
+    <h2>Workspaces</h2>
+    <div>
       {#await workspaces}
         loading workspaces...
       {:then workspaces}
         <ul>
           {#each workspaces as workspace}
-            <li>{workspace.id} - {workspace.root}</li>
+            <li
+              class="a"
+              on:click={() => {
+                router.push(`/workspaces/${encodeURIComponent(workspace.id)}`);
+              }}
+            >
+              <b>{workspace.id}</b>
+              <span>{workspace.root}</span>
+            </li>
           {/each}
         </ul>
       {:catch error}
@@ -29,5 +39,58 @@
 <style>
   section {
     padding: 30px;
+  }
+
+  h2 {
+    margin-top: 36px;
+    margin-bottom: 24px;
+  }
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: grid;
+    gap: 16px;
+    grid-template-columns: repeat(
+      auto-fill,
+      minmax(320px, 1fr)
+    ); /* Auto-responsive */
+  }
+
+  li {
+    background: linear-gradient(#fff, #f5f5f5);
+    border: none;
+    border-radius: 6px;
+    padding: 16px 24px;
+    box-shadow: 0 0.33px 0 1px hsla(0, 0%, 100%, 0.15),
+      0 4px 8px -3px rgba(0, 0, 0, 0.15), 0 0.4px 0 0.8px rgba(0, 0, 0, 0.25);
+    position: relative;
+    display: grid;
+    grid-template-columns: max-content 2fr;
+    align-items: center;
+    gap: 12px;
+  }
+
+  li > b {
+    max-width: 6em;
+  }
+
+  li > * {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+
+  li:hover {
+    background: linear-gradient(#fafafa, #e7e7e7);
+    box-shadow: 0 0.33px 0 1px hsla(0, 0%, 100%, 0.15),
+      0 6px 8px -4px rgba(0, 0, 0, 0.2), 0 0.4px 0 0.8px rgba(0, 0, 0, 0.35);
+  }
+
+  li:active {
+    background: linear-gradient(#f7f7f7, #e0e0e0);
+    box-shadow: 0 0.33px 0 1px hsla(0, 0%, 100%, 0.15),
+      0 4px 6px -3px rgba(0, 0, 0, 0.15), 0 0.4px 0 0.8px rgba(0, 0, 0, 0.45);
   }
 </style>
