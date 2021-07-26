0fcccb0aa0e94ee02444308933f1f603e340723f
Brandon Bloom <brandon@brandonbloom.name>
2021-07-26 14:59:43 -0700

    call describe-workspaces from client

--- gui/src/pages/Home.svelte
+++ gui/src/pages/Home.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
   import Layout from '../components/Layout.svelte';
   import Code from '../components/Code.svelte';
+  import { api } from '../lib/api';
+
+  const workspaces = api.kernel.describeWorkspaces();
 </script>
 
 <Layout>
   <section>
     Use <Code>exo gui</Code> in your terminal.
+    <!-- TODO: Use this data below: !-->
+    <div style="display: none">
+      {#await workspaces}
+        loading workspaces...
+      {:then workspaces}
+        <ul>
+          {#each workspaces as workspace}
+            <li>{workspace.id} - {workspace.root}</li>
+          {/each}
+        </ul>
+      {:catch error}
+        <p style="color: red">{error.message}</p>
+      {/await}
+    </div>
   </section>
 </Layout>
 
