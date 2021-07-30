018d03a505e7bed10d837c45902dffad0fccab6f
John <51100181+jwmza@users.noreply.github.com>
2021-07-30 14:23:30 -0400

    Add a test page for log messages

--- gui/src/routes.ts
+++ gui/src/routes.ts
@@ -3,11 +3,13 @@ import Workspace from './pages/Workspace.svelte';
 import NewWorkspace from './pages/NewWorkspace.svelte';
 import NewProcess from './pages/NewProcess.svelte';
 import NotFound from './pages/NotFound.svelte';
+import LogMessages from './test/LogMessages.svelte';
 
 export default {
   '/': Home,
   '/new-workspace': NewWorkspace,
   '/workspaces/:workspace': Workspace,
   '/workspaces/:workspace/new-process': NewProcess,
+  '/test/logs': LogMessages,
   '*': NotFound,
 };
---
+++ gui/src/test/LogMessages.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Layout from '../components/Layout.svelte';
+</script>
+
+<Layout>
+  <section>
+    <h1>Links</h1>
+    <p>test</p>
+  </section>
+</Layout>
+
+<style>
+</style>
