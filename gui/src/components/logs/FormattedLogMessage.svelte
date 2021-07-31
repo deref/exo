f5515d859ffa5e8b47f87fa2aaf2f160f788b675
John <51100181+jwmza@users.noreply.github.com>
2021-07-30 23:30:51 -0400

    Improve parsing with character loops

--- gui/src/components/logs/FormattedLogMessage.svelte
+++ gui/src/components/logs/FormattedLogMessage.svelte
@@ -1,19 +1,41 @@
 <script lang="ts">
   export let message: string = '';
 
-  let words = message.split(' ');
-
+  const whitespaceRegex = /\s/;
   const linkRegex = /^https*:\/\/\S+\.\S+/;
+
+  // Substring buckets of whitespace and non whitespace spans
+  let buckets = [message[0]];
+
+  for (let i = 1; i < message.length; i++) {
+    if (
+      whitespaceRegex.test(message[i]) !== whitespaceRegex.test(message[i - 1])
+    ) {
+      buckets.push(message[i]);
+    } else {
+      buckets[buckets.length - 1] += message[i];
+    }
+  }
+
+  // Substring spans of links and non-links
+  let spans = [buckets[0]];
+
+  for (let i = 1; i < buckets.length; i++) {
+    if (linkRegex.test(buckets[i]) !== linkRegex.test(buckets[i - 1])) {
+      spans.push(buckets[i]);
+    } else {
+      spans[spans.length - 1] += buckets[i];
+    }
+  }
 </script>
 
 <span>
-  {#each words as word}
-    {#if linkRegex.test(word)}
-      <a href={word}>{word}</a>
+  {#each spans as span}
+    {#if linkRegex.test(span)}
+      <a href={span}>{span}</a>
     {:else}
-      <span>{word}</span>
+      <span>{span}</span>
     {/if}
-    {' '}
   {/each}
 </span>
 
