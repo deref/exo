64d9a93209027196bc8adae5f62d03fde9cbe4e5
Brandon Bloom <brandon@brandonbloom.name>
2021-08-05 14:57:25 -0700

    ignore .git directory changes in server watcher

--- script/dev-server.sh
+++ script/dev-server.sh
@@ -3,5 +3,5 @@
 source "$( dirname "${BASH_SOURCE[0]}" )/.include"
 
 env EXO_HOME="$EXO_DEV_HOME" \
-    go run ./cmd/watch --dir "${ROOT_DIR}" -r yes --ignore 'var,gui' -- \
+    go run ./cmd/watch --dir "${ROOT_DIR}" -r yes --ignore '.git,var,gui' -- \
         go run "${ROOT_DIR}/cmd/exo" server --force-std-log=1
