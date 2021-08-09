f926cc8e4ca1d86873d12796bfca6624cd2ba5ab
Andrew Meredith <andymeredith@gmail.com>
2021-08-09 17:04:41 -0600

    Get basic procfile generation working

--- internal/external/procfile/generate.go
+++ internal/external/procfile/generate.go
@@ -1,5 +1,48 @@
 package procfile
 
+import (
+	"bytes"
+
+	"gopkg.in/alessio/shellescape.v1"
+	"mvdan.cc/sh/v3/syntax"
+)
+
 func Generate(procs []Process) (string, error) {
-	return "test generate", nil
+	var out bytes.Buffer
+
+	for _, proc := range procs {
+		out.WriteString(proc.Name)
+		out.WriteString(": ")
+
+		cmd := &syntax.CallExpr{
+			Args: []*syntax.Word{
+				literalWord(proc.Program),
+			},
+		}
+		for _, arg := range proc.Arguments {
+			cmd.Args = append(cmd.Args, literalWord(arg))
+		}
+
+		for key, val := range proc.Environment {
+			cmd.Assigns = append(cmd.Assigns, &syntax.Assign{
+				Name:  &syntax.Lit{Value: key},
+				Value: literalWord(val),
+			})
+		}
+
+		syntax.NewPrinter().Print(&out, &syntax.Stmt{
+			Cmd: cmd,
+		})
+		out.WriteByte('\n')
+	}
+
+	return out.String(), nil
+}
+
+func literalWord(val string) *syntax.Word {
+	return &syntax.Word{
+		Parts: []syntax.WordPart{
+			&syntax.Lit{Value: shellescape.Quote(val)},
+		},
+	}
 }
