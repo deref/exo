39e660ce75d75ddb91dd355deb89d7fe75e65c69
Andrew Meredith <andymeredith@gmail.com>
2021-08-11 13:20:05 -0600

    Revert "Revert "Merge pull request #118 from deref/operation-telemetry""
    This reverts commit 77eef10a377e031dcb09f9b11323d5941927cbd6.

--- internal/telemetry/default.go
+++ internal/telemetry/default.go
@@ -2,22 +2,31 @@ package telemetry
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"unsafe"
 
 	"github.com/deref/exo"
 	"github.com/deref/exo/internal/config"
 	"github.com/deref/exo/internal/util/cacheutil"
+	"github.com/deref/exo/internal/util/logging"
 )
 
 type defaultTelemetry struct {
-	cfg       *config.TelemetryConfig
-	client    *http.Client
-	sessionID string
+	ctx            context.Context
+	cfg            *config.TelemetryConfig
+	client         *http.Client
+	sessionID      string
+	operationGauge *SummaryGauge
 
 	latestVersion *cacheutil.TTLVal
+	getSession    sync.Once
 }
 
 func (t *defaultTelemetry) IsEnabled() bool {
@@ -26,43 +35,111 @@ func (t *defaultTelemetry) IsEnabled() bool {
 	return true
 }
 
-func (t *defaultTelemetry) StartSession() {
-	t.ensureSession()
+func (t *defaultTelemetry) StartSession(ctx context.Context) {
+	t.ensureSession(ctx)
 }
 
-func (t *defaultTelemetry) LatestVersion() (string, error) {
-	v, err := t.latestVersion.Get()
+func (t *defaultTelemetry) LatestVersion(ctx context.Context) (string, error) {
+	v, err := t.latestVersion.Get(ctx)
 	if err != nil {
 		return "", err
 	}
 	return v.(string), nil
 }
 
-func (t *defaultTelemetry) SendEvent(evt event) {
-	t.ensureSession()
-	var buf bytes.Buffer
-	req := telemetryRequest{
-		Method: "record-event",
-		Data: map[string]interface{}{
-			"id":      evt.ID(),
-			"payload": evt.Payload(),
-		},
-	}
-	e := json.NewEncoder(&buf)
-	e.Encode(req)
-	// Ignore response.
-	_, _ = t.client.Post(exo.TelemetryEndpoint, "application/json", &buf)
+func (t *defaultTelemetry) SendEvent(ctx context.Context, evt event) {
+	// TODO: Limit concurrent sends/throttle.
+	go func() {
+		t.ensureSession(ctx)
+		var buf bytes.Buffer
+		req := telemetryRequest{
+			Method: "record-event",
+			Data: map[string]interface{}{
+				"id":        evt.ID(),
+				"payload":   evt.Payload(),
+				"sessionId": t.sessionID,
+			},
+		}
+		e := json.NewEncoder(&buf)
+		e.Encode(req)
+		// Ignore response.
+		_, _ = t.client.Post(exo.TelemetryEndpoint, "application/json", &buf)
+	}()
 }
 
-func (t *defaultTelemetry) ensureSession() {
-	if t.sessionID == "" {
-		t.startSession()
+func (t *defaultTelemetry) RecordOperation(op OperationInvocation) {
+	var success string
+	if op.Success {
+		success = "y"
+	} else {
+		success = "n"
 	}
+
+	t.operationGauge.Observe(Tags{
+		"operation": op.Operation,
+		"success":   success,
+	}, float64(op.DurationMicros))
 }
 
-func (t *defaultTelemetry) startSession() {
-	// TODO: Start a session at the telemetry endpoint, and update the internal sessionID.
-	// This needs to be thread-safe.
+type startSessionResponse struct {
+	SessionID string `json:"sessionId"`
+}
+
+func (t *defaultTelemetry) ensureSession(ctx context.Context) {
+	if t.sessionID != "" {
+		return
+	}
+
+	t.getSession.Do(func() {
+		logger := logging.CurrentLogger(ctx)
+		errSetSession := func(res *http.Response, err error) {
+			var msg strings.Builder
+			msg.WriteString("Error creating session. Setting null UUID.")
+			if res != nil && res.StatusCode > 0 {
+				var responseText string
+				if responseBody, resErr := ioutil.ReadAll(res.Request.Body); resErr != nil {
+					responseText = "cannot read response"
+				} else {
+					responseText = string(responseBody)
+				}
+				msg.WriteString(fmt.Sprintf(" - HTTP %d: %q", res.StatusCode, responseText))
+			}
+
+			if err != nil {
+				msg.WriteString(fmt.Sprintf(": %v", err))
+			}
+
+			logger.Infof(msg.String())
+			t.sessionID = "00000000-0000-0000-0000-000000000000"
+		}
+
+		var buf bytes.Buffer
+		req := telemetryRequest{
+			Method: "start-session",
+		}
+		e := json.NewEncoder(&buf)
+		e.Encode(req)
+		res, err := t.client.Post(exo.TelemetryEndpoint, "application/json", &buf)
+		if err != nil {
+			errSetSession(res, err)
+			return
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode != 200 {
+			errSetSession(res, nil)
+			return
+		}
+
+		typedRes := startSessionResponse{}
+		if err := json.NewDecoder(res.Body).Decode(&typedRes); err != nil {
+			errSetSession(res, err)
+			return
+		}
+
+		t.sessionID = typedRes.SessionID
+		logger.Infof("Started session: %q", t.sessionID)
+	})
 }
 
 func (t *defaultTelemetry) getLatestVersion() (interface{}, error) {
@@ -76,4 +153,34 @@ func (t *defaultTelemetry) getLatestVersion() (interface{}, error) {
 	}
 
 	return string(body), nil
+}
+
+func (t *defaultTelemetry) sendRecordedTelemetry() {
+	if len(t.operationGauge.buckets) == 0 {
+		return
+	}
+
+	newGauge := newOperationGauge()
+	oldPtr := (*unsafe.Pointer)(unsafe.Pointer(&t.operationGauge))
+	swappedPtr := atomic.SwapPointer(oldPtr, unsafe.Pointer(newGauge))
+	g := (*SummaryGauge)(swappedPtr)
+
+	logging.CurrentLogger(t.ctx).Infof("Sending telemetry")
+
+	for _, bucket := range g.buckets {
+		tags := bucket.Tags()
+		op := tags["operation"]
+		success := tags["success"] == "y"
+		summary := bucket.Summarize()
+
+		t.SendEvent(t.ctx, &OperationsPerformed{
+			Operation:       op,
+			Success:         success,
+			DurationSummary: summary,
+		})
+	}
+}
+
+func newOperationGauge() *SummaryGauge {
+	return NewSummaryGauge([]string{"operation", "success"})
 }
--- internal/telemetry/event.go
+++ internal/telemetry/event.go
@@ -1,5 +1,7 @@
 package telemetry
 
+import "github.com/deref/exo"
+
 type event interface {
 	ID() string
 	Payload() map[string]interface{}
@@ -21,5 +23,24 @@ func (e *SystemInfoIdentified) Payload() map[string]interface{} {
 	return map[string]interface{}{
 		"some": "data",
 		"goes": "here",
+	}
+}
+
+type OperationsPerformed struct {
+	Operation       string
+	Success         bool
+	DurationSummary SummaryStatistics
+}
+
+func (e *OperationsPerformed) ID() string {
+	return "a20fc050-ba26-4f05-832a-7d42283d1888"
+}
+
+func (e *OperationsPerformed) Payload() map[string]interface{} {
+	return map[string]interface{}{
+		"exoVersion": exo.Version,
+		"operation":  e.Operation,
+		"success":    e.Success,
+		"summary":    e.DurationSummary,
 	}
 }
