3aadaf6cb0b3f5f5c180e5e1a5a3d8e0a5cbe8ba
Brandon Bloom <brandon@brandonbloom.name>
2021-08-10 18:02:29 -0700

    enable docker-compose discovery

--- cmd/exo/apply.go
+++ cmd/exo/apply.go
@@ -23,42 +23,27 @@ var applyCmd = &cobra.Command{
 	Use:   "apply [flags] [manifest-file]",
 	Short: "Applies a manifest in the current workspace",
 	Long: `Applies a manifest in the current workspace.
-
-If no manifest file is specified, a search is conducted in the current directory
-in the following order of format preference:
-
-  1. exo
-	2. procfile
 	
-The default exo filename is 'exo.hcl'.
-
-The expected procfile name 'Procfile'.
-
-If a manifest format will be guessed from the manifest filename.  This can be
-overidden explicitly with the --format flag.`,
-	// TODO: Replace docs when we have docker-compose support.
-	//	Long: `Applies a manifest in the current workspace.
-	//
-	//If no manifest file is specified, a search is conducted in the current directory
-	//in the following order of format preference:
-	//
-	//  1. exo
-	//	2. compose
-	//	3. procfile
-	//
-	//The default exo filename is 'exo.hcl'.
-	//
-	//Compose files may have one of the following names in order of preference:
-	//
-	//	compose.yaml
-	//	compose.yml
-	//  docker-compose.yaml
-	//  docker-compose.yml
-	//
-	//The expected procfile name 'Procfile'.
-	//
-	//If a manifest format will be guessed from the manifest filename.  This can be
-	//overidden explicitly with the --format flag.`,
+	If no manifest file is specified, a search is conducted in the current directory
+	in the following order of format preference:
+	
+	  1. exo
+		2. compose
+		3. procfile
+	
+	The default exo filename is 'exo.hcl'.
+	
+	Compose files may have one of the following names in order of preference:
+	
+		compose.yaml
+		compose.yml
+	  docker-compose.yaml
+	  docker-compose.yml
+	
+	The expected procfile name 'Procfile'.
+	
+	If a manifest format will be guessed from the manifest filename.  This can be
+	overidden explicitly with the --format flag.`,
 	Args: cobra.MaximumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		ctx := newContext()
