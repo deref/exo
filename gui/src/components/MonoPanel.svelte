34ed72eed05e5195bac151c39b9c4338d6eb1abc
John <51100181+jwmza@users.noreply.github.com>
2021-08-12 08:54:34 -0400

    Improve MonoPanel styling

--- gui/src/components/MonoPanel.svelte
+++ gui/src/components/MonoPanel.svelte
@@ -5,9 +5,10 @@
 <style>
   section {
     position: relative;
-    overflow-y: auto;
+    overflow-y: scroll;
     overflow-x: hidden;
     background: var(--primary-bg-color);
+    padding: var(--panel-padding);
     height: 100vh;
   }
 </style>
