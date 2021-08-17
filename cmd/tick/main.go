2529e3b027c791c1cbc0bf6729f1e92f326428fa
Andrew Meredith <andymeredith@gmail.com>
2021-08-17 14:28:23 -0600

    Add --interval-ms flag to tick

--- cmd/tick/main.go
+++ cmd/tick/main.go
@@ -4,14 +4,31 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"time"
+
+	"github.com/deref/exo/internal/util/cmdutil"
 )
 
 func main() {
+	cmd, err := cmdutil.ParseArgs(os.Args)
+	if err != nil {
+		cmdutil.Fatalf("parsing arguments: %w", err)
+	}
+
+	timeoutMS := 1000
+	if timeoutFlag, ok := cmd.Flags["timeout-ms"]; ok {
+		var err error
+		if timeoutMS, err = strconv.Atoi(timeoutFlag); err != nil {
+			cmdutil.Fatalf("parsing --timeout-ms: %w", err)
+		}
+	}
+
 	i := 0
 	for {
 		i++
 		fmt.Printf("tick %d at %v\n", i, time.Now())
-		<-time.After(1 * time.Second)
+		<-time.After(time.Duration(timeoutMS) * time.Millisecond)
 	}
 }
