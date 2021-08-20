e0885be0766da7e5dcf16a8f331da8dcade72599
Ben Elgar <me@benelgar.com>
2021-08-20 13:49:31 +0100

    Accept more formats when parsing docker commands (#239)

--- internal/providers/docker/compose/command.go
+++ internal/providers/docker/compose/command.go
@@ -1,7 +1,14 @@
 package compose
 
-import "mvdan.cc/sh/v3/shell"
+import (
+	"github.com/mattn/go-shellwords"
+)
 
+// FIXME: this incorrectly treats the "command" passed to docker as a
+// meaningful shell command that can be parsed into "shell words". In reality
+// this is an arbitrary string that is passed to the entrypoint of the
+// container as an argument. It could be a shell command but it could also be
+// a python program or a Dan Brown novel.
 type Command []string
 
 func (cmd Command) MarshalYAML() (interface{}, error) {
@@ -21,8 +28,7 @@ func (cmd *Command) UnmarshalYAML(unmarshal func(interface{}) error) error {
 		return err
 	}
 
-	var noEnv func(string) string
-	strs, err = shell.Fields(s, noEnv)
+	strs, err = shellwords.Parse(s)
 	*cmd = Command(strs)
 	return err
 }
