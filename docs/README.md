fbcb0b8a24e8043e680f09e062529cf0c51e65a6
John <51100181+jwmza@users.noreply.github.com>
2021-09-10 15:55:33 -0400

    Initialize Next app

---
+++ docs/README.md
@@ -0,0 +1,3 @@
+This is a Next.js app intended to serve as a documentation website for exo.
+
+Run `npm run dev` and open `localhost:3000` to get started.
---
+++ docs/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default Home;
