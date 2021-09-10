0cae7e4f26cfbab37eea5e4f7f393411bf38fcb3
John <51100181+jwmza@users.noreply.github.com>
2021-09-10 16:47:04 -0400

    Add custom stylesheet to docs

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,5 +1,5 @@
-// import '../styles/globals.css'
 import "nextra-theme-docs/style.css";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function Nextra({ Component, pageProps }: AppProps) {
