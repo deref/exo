6c5d16115cb13733c9b97698f70dcedd0a352d12
Ben Elgar <me@benelgar.com>
2021-09-24 11:17:16 +0100

    Tidy URL query handling

--- gui/dev.go
+++ gui/dev.go
@@ -17,8 +17,7 @@ import (
 
 var tokenCookieMiddleware = func(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		query := req.URL.Query()
-		token := query.Get("token")
+		token := req.URL.Query().Get("token")
 		if token == "" {
 			next.ServeHTTP(w, req)
 			return
@@ -32,9 +31,10 @@ var tokenCookieMiddleware = func(next http.Handler) http.Handler {
 			SameSite: http.SameSiteLaxMode,
 			HttpOnly: true,
 		})
-		query.Del("token")
 		redirectURL := req.URL
-		redirectURL.RawQuery = query.Encode()
+		redirectQuery := redirectURL.Query()
+		redirectQuery.Del("token")
+		redirectURL.RawQuery = redirectQuery.Encode()
 		http.Redirect(w, req, redirectURL.String(), http.StatusTemporaryRedirect)
 	})
 }
