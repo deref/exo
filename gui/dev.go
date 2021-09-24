46827421fc8755994a2ee7c62f51ed17164204b4
Ben Elgar <me@benelgar.com>
2021-09-24 11:12:07 +0100

    Move cookie setting to server and set params

--- gui/dev.go
+++ gui/dev.go
@@ -8,11 +8,36 @@ import (
 	"net/http"
 	goutil "net/http/httputil"
 	"net/url"
+	"time"
 
 	"github.com/deref/exo/internal/config"
 	"github.com/deref/exo/internal/util/errutil"
 	exoutil "github.com/deref/exo/internal/util/httputil"
 )
+
+var tokenCookieMiddleware = func(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		query := req.URL.Query()
+		token := query.Get("token")
+		if token == "" {
+			next.ServeHTTP(w, req)
+			return
+		}
+
+		http.SetCookie(w, &http.Cookie{
+			Name:     "token",
+			Value:    token,
+			Expires:  time.Now().AddDate(1, 0, 0), // 1 year
+			Secure:   true,
+			SameSite: http.SameSiteLaxMode,
+			HttpOnly: true,
+		})
+		query.Del("token")
+		redirectURL := req.URL
+		redirectURL.RawQuery = query.Encode()
+		http.Redirect(w, req, redirectURL.String(), http.StatusTemporaryRedirect)
+	})
+}
 
 func NewHandler(ctx context.Context, cfg config.GUIConfig) http.Handler {
 	guiURL, err := url.Parse(fmt.Sprintf("http://localhost:%d/", cfg.Port))
@@ -24,5 +49,5 @@ func NewHandler(ctx context.Context, cfg config.GUIConfig) http.Handler {
 		err = errutil.NewHTTPError(http.StatusBadGateway, err.Error())
 		exoutil.WriteError(w, req, err)
 	}
-	return proxy
+	return tokenCookieMiddleware(proxy)
 }
--- gui/src/App.svelte
+++ gui/src/App.svelte
@@ -3,14 +3,6 @@
   import Router from 'svelte-spa-router';
   import Offline from './pages/Offline.svelte';
   import { isRunning } from './lib/global/server-status';
-
-  const params = new URLSearchParams(window.location.search);
-  const urlToken = params.get('token');
-  if (urlToken) {
-    document.cookie = `token=${urlToken}`;
-    params.delete('token');
-    window.location.search = params.toString();
-  }
 </script>
 
 {#if $isRunning}
