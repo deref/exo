60842855b39a97a1ac93ce7795d67f46ed115c46
Brandon Bloom <brandon@brandonbloom.name>
2021-09-27 13:53:16 -0700

    disable secure cookie on localhost

--- gui/tokenmiddleware.go
+++ gui/tokenmiddleware.go
@@ -2,29 +2,33 @@ package gui
 
 import (
 	"net/http"
+	"strings"
 	"time"
 )
 
-var tokenCookieMiddleware = func(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		token := req.URL.Query().Get("token")
-		if token == "" {
-			next.ServeHTTP(w, req)
-			return
-		}
+type tokenCookieMiddleware struct {
+	URL  string
+	Next http.Handler
+}
 
-		http.SetCookie(w, &http.Cookie{
-			Name:     "token",
-			Value:    token,
-			Expires:  time.Now().AddDate(1, 0, 0), // 1 year
-			Secure:   true,
-			SameSite: http.SameSiteLaxMode,
-			HttpOnly: true,
-		})
-		redirectURL := req.URL
-		redirectQuery := redirectURL.Query()
-		redirectQuery.Del("token")
-		redirectURL.RawQuery = redirectQuery.Encode()
-		http.Redirect(w, req, redirectURL.String(), http.StatusTemporaryRedirect)
+func (m *tokenCookieMiddleware) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	token := req.URL.Query().Get("token")
+	if token == "" {
+		m.Next.ServeHTTP(w, req)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "token",
+		Value:    token,
+		Expires:  time.Now().AddDate(1, 0, 0), // 1 year
+		Secure:   !strings.HasPrefix(m.URL, "http:"),
+		SameSite: http.SameSiteLaxMode,
+		HttpOnly: true,
 	})
+	redirectURL := req.URL
+	redirectQuery := redirectURL.Query()
+	redirectQuery.Del("token")
+	redirectURL.RawQuery = redirectQuery.Encode()
+	http.Redirect(w, req, redirectURL.String(), http.StatusTemporaryRedirect)
 }
