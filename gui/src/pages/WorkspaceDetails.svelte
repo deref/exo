58b2b2b6c59f00d90ebb95aa25f158db5a53ad57
John <51100181+jwmza@users.noreply.github.com>
2021-09-27 19:46:02 -0400

    Use workspace `displayName` in details page title

--- gui/src/pages/WorkspaceDetails.svelte
+++ gui/src/pages/WorkspaceDetails.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import Panel from '../components/Panel.svelte';
   import Layout from '../components/Layout.svelte';
-  import Spinner from '../components/Spinner.svelte';
   import WorkspaceNav from '../components/WorkspaceNav.svelte';
   import CheckeredTableWrapper from '../components/CheckeredTableWrapper.svelte';
   import { api } from '../lib/api';
@@ -15,10 +14,13 @@
 
 <Layout>
   <WorkspaceNav {workspaceId} active="Dashboard" slot="navbar" />
-  <Panel title="Workspace details" backRoute={workspaceRoute}>
-    {#await workspace.describeSelf()}
-      <Spinner />
-    {:then description}
+  {#await workspace.describeSelf()}
+    <Panel title="Loading..." backRoute={workspaceRoute} />
+  {:then description}
+    <Panel
+      title={description.displayName + '/Details'}
+      backRoute={workspaceRoute}
+    >
       <CheckeredTableWrapper>
         <table>
           <tbody>
@@ -37,10 +39,8 @@
           </tbody>
         </table>
       </CheckeredTableWrapper>
-    {/await}
-
-    <!-- Databases, Apps, cloud services, etc. -->
-  </Panel>
+    </Panel>
+  {/await}
 </Layout>
 
 <style>
