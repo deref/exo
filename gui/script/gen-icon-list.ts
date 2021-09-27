46116071967a83534b9e3ba67f7cec1e0d05dad8
Brandon Bloom <brandon@brandonbloom.name>
2021-09-27 14:31:31 -0700

    Add `./script/codegen.sh` and `(cd gui && npm run codegen)`

--- gui/script/gen-icon-list.ts
+++ gui/script/gen-icon-list.ts
@@ -1,11 +1,9 @@
-const fs = require("fs");
+import fs from 'fs';
 
-// Run `/path/to/exo $ node script/gen-icon-list.js` to generate output.
-
-const iconDirectory = "mono";
+const iconDirectory = 'mono';
 const iconNames = fs
-  .readdirSync(`./gui/src/components/${iconDirectory}`)
-  .map((x) => x.substr(0, x.length - "SVG.svelte".length));
+  .readdirSync(`./src/components/${iconDirectory}`)
+  .map((x) => x.substr(0, x.length - 'SVG.svelte'.length));
 
 // Use maps to format the file.
 const out = `<script lang="ts" context="module">
@@ -14,27 +12,27 @@ const out = `<script lang="ts" context="module">
   // Note, these are internal named SVG icons. Dynamic SVGs should be handled separately.
 
   export type IconGlyph =
-${iconNames.map((x) => `    | '${x}'`).join("\n")};
+${iconNames.map((x) => `    | '${x}'`).join('\n')};
 </script>
 
 <script lang="ts">
 ${iconNames
   .map((x) => `  import ${x}SVG from './${iconDirectory}/${x}SVG.svelte';`)
-  .join("\n")}
+  .join('\n')}
 
   export let glyph: IconGlyph;
 </script>
 
 {#${iconNames
   .map((x) => `if glyph === '${x}'}\n  <${x}SVG />`)
-  .join("\n{:else ")}
+  .join('\n{:else ')}
 {:else}
   <LayersSVG />
 {/if}
 `;
 
 // Save generated output.
-fs.writeFile("./gui/src/components/Icon.svelte", out, function (err) {
+fs.writeFile('./src/components/Icon.svelte', out, function (err) {
   if (err) throw err;
-  console.log("Generated Icon.svelte.");
+  console.log('Generated Icon.svelte.');
 });
