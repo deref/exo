e6385e2047fe0adb82e3774c2e64cba4884a158d
Ben Elgar <me@benelgar.com>
2021-10-22 12:38:22 +0100

    Update docs

--- gui/vite.config.js
+++ gui/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // Vite server runs on 3001 in development, which is proxied from the
-// `exo` server running on 4001. In production, exo serves the GUI and
+// `exo` server running on 44643. In production, exo serves the GUI and
 // the API on port 43643.
 const port = 3001;
 
