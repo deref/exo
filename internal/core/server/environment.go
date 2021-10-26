b9f07f6fee8d90faf1ba5080677948b2cdac3992
Ben Elgar <me@benelgar.com>
2021-10-26 13:41:16 +0100

    Prevent failure when cannot retrieve secrets (#439)

--- internal/core/server/environment.go
+++ internal/core/server/environment.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/deref/exo/internal/core/api"
 	"github.com/deref/exo/internal/environment"
+	"github.com/deref/exo/internal/util/logging"
 	"github.com/deref/exo/internal/util/osutil"
 )
 
@@ -13,18 +14,6 @@ import (
 // it is no longer appropriate to call deep in the call stack.
 func (ws *Workspace) getEnvironment(ctx context.Context) (map[string]api.VariableDescription, error) {
 	var sources []environment.Source
-
-	describeVaultsResult, err := ws.DescribeVaults(ctx, &api.DescribeVaultsInput{})
-	if err != nil {
-		return nil, fmt.Errorf("getting vaults: %w", err)
-	}
-	for _, vault := range describeVaultsResult.Vaults {
-		sources = append(sources, &environment.ESV{
-			Client: ws.EsvClient,
-			Name:   vault.Name,
-			URL:    vault.Url,
-		})
-	}
 
 	if manifest := ws.tryLoadManifest(ctx); manifest != nil {
 		sources = append(sources, manifest.Environment())
@@ -48,6 +37,29 @@ func (ws *Workspace) getEnvironment(ctx context.Context) (map[string]api.Variabl
 	b := &environmentBuilder{
 		Environment: make(map[string]api.VariableDescription),
 	}
+
+	describeVaultsResult, err := ws.DescribeVaults(ctx, &api.DescribeVaultsInput{})
+	if err != nil {
+		return nil, fmt.Errorf("getting vaults: %w", err)
+	}
+
+	logger := logging.CurrentLogger(ctx)
+	for _, vault := range describeVaultsResult.Vaults {
+		derefSource := &environment.ESV{
+			Client: ws.EsvClient,
+			Name:   vault.Name,
+			URL:    vault.Url,
+		}
+		if err := derefSource.ExtendEnvironment(b); err != nil {
+			// It's not appropriate to fail on error since this error could just
+			// indicate the user is offline and thus cannot retrieve this value from
+			// the secret provider.
+			// TODO: this should really alert the user in a more apparent way that
+			// fetching secrets from the vault has failed.
+			logger.Infof("Could not extend environment from vault %s: %q", vault.Name, err)
+		}
+	}
+
 	for _, source := range sources {
 		if err := source.ExtendEnvironment(b); err != nil {
 			return nil, fmt.Errorf("extending environment from %s: %w", source.EnvironmentSource(), err)
