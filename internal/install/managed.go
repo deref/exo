b6f6070c1832b444a5ec39855722b4fa5f0ae542
Ben Elgar <me@benelgar.com>
2021-10-27 15:03:26 +0100

    Fix bad function signature in release build

--- internal/install/managed.go
+++ internal/install/managed.go
@@ -6,7 +6,7 @@ import "fmt"
 
 const IsManaged = true
 
-func (i *Install) UpgradeSelf(deviceID string) error {
+func (i *Install) UpgradeSelf() error {
 	fmt.Println(`This version of exo was installed via a package manager and does not support self-upgrade. Please use your package manager to update.
 
 Alternatively, you may uninstall exo using your system's package manager and download the latest version with:
