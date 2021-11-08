37f9b082d54c838b97698553f6a61ccc14f72671
Ben Elgar <me@benelgar.com>
2021-11-08 16:21:17 +0000

    Separate running tests from tests (#487)

--- test/main.go
+++ test/main.go
@@ -6,57 +6,12 @@ import (
 	"io/ioutil"
 	"os"
 	"sync"
-	"time"
 
 	"github.com/deref/exo/test/tester"
 )
 
 type exoTestParams struct {
 	exoBinary string
-}
-
-var basicT0Test = func(ctx context.Context, t tester.ExoTester) error {
-	if _, _, err := t.RunExo(ctx, "init"); err != nil {
-		return err
-	}
-	if _, _, err := t.RunExo(ctx, "start"); err != nil {
-		return err
-	}
-	ctx, cancel := context.WithDeadline(ctx, time.Now().Add(time.Second*5))
-	defer cancel()
-	return t.WaitTillProcessesRunning(ctx, "t0")
-}
-
-var tests = map[string]tester.ExoTest{
-	"basic-procfile": {
-		FixtureDir: "basic-procfile",
-		Test:       basicT0Test,
-	},
-	"basic-dockerfile": {
-		FixtureDir: "basic-dockerfile",
-		Test:       basicT0Test,
-	},
-	"basic-exo-hcl": {
-		FixtureDir: "basic-exo-hcl",
-		Test:       basicT0Test,
-	},
-	"simple-example": {
-		FixtureDir: "simple-example",
-		Test: func(ctx context.Context, t tester.ExoTester) error {
-			if _, _, err := t.RunExo(ctx, "init"); err != nil {
-				return err
-			}
-			if _, _, err := t.RunExo(ctx, "start"); err != nil {
-				return err
-			}
-			ctx, cancel := context.WithDeadline(ctx, time.Now().Add(time.Second*10))
-			defer cancel()
-			if err := t.WaitTillProcessesRunning(ctx, "web", "echo", "echo-short"); err != nil {
-				return err
-			}
-			return nil
-		},
-	},
 }
 
 func doTest(ctx context.Context, test tester.ExoTest, testName, exoBinPath, fixtureBasePath string, outputMutex *sync.Mutex) error {
