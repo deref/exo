3862ca0b49cdea58f585c5b4d3c2453ad50faad2
Brandon Bloom <brandon.d.bloom@gmail.com>
2022-01-11 11:24:27 -0800

    scaffold out some more gql

---
+++ internal/resolvers/component.go
@@ -0,0 +1,33 @@
+package resolvers
+
+import "context"
+
+type ComponentResolver struct {
+	Q *QueryResolver
+	ComponentRow
+}
+
+type ComponentRow struct {
+	ID      string `db:"id"`
+	StackID string `db:"stack_id"`
+	Name    string `db:"name"`
+}
+
+func (r *QueryResolver) ComponentByID(ctx context.Context, args struct {
+	ID string
+}) (*ComponentResolver, error) {
+	return r.componentByID(ctx, &args.ID)
+}
+
+func (r *QueryResolver) componentByID(ctx context.Context, id *string) (*ComponentResolver, error) {
+	s := &ComponentResolver{}
+	err := r.getRowByID(ctx, &s.ComponentRow, `
+		SELECT id, stack_id, name
+		FROM component
+		WHERE id = ?
+	`, id)
+	if s.ID == "" {
+		s = nil
+	}
+	return s, err
+}
