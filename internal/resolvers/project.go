72d2d56186b2003483738468c20a90437daa6ad6
Brandon Bloom <brandon.d.bloom@gmail.com>
2022-01-11 11:54:32 -0800

    more gql stuff

--- internal/resolvers/project.go
+++ internal/resolvers/project.go
@@ -12,6 +12,26 @@ type ProjectRow struct {
 	DisplayName *string `db:"display_name"`
 }
 
+func (r *QueryResolver) AllProjects(ctx context.Context) ([]*ProjectResolver, error) {
+	var rows []ProjectRow
+	err := r.DB.SelectContext(ctx, &rows, `
+		SELECT id, display_name
+		FROM project
+		ORDER BY id ASC
+	`)
+	if err != nil {
+		return nil, err
+	}
+	resolvers := make([]*ProjectResolver, len(rows))
+	for i, row := range rows {
+		resolvers[i] = &ProjectResolver{
+			Q:          r,
+			ProjectRow: row,
+		}
+	}
+	return resolvers, nil
+}
+
 func (r *QueryResolver) ProjectByID(ctx context.Context, args struct {
 	ID string
 }) (*ProjectResolver, error) {
