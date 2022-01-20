6d8cfdcc191b20ec35034c851fe631587c2cb0c5
Brandon Bloom <brandon.d.bloom@gmail.com>
2022-01-19 16:22:49 -0800

    cli json command

---
+++ internal/cli/json.go
@@ -0,0 +1,56 @@
+package cli
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/deref/exo/internal/util/cmdutil"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(jsonCmd)
+	jsonCmd.Flags().BoolVar(&jsonFlags.Indent, "indent", false, "output indented")
+}
+
+var jsonFlags struct {
+	Indent bool
+}
+
+var jsonCmd = &cobra.Command{
+	Use:    "json [properties...]",
+	Hidden: true,
+	Short:  "Builds a JSON object.",
+	Long: `Builds a JSON object of the properties specified as command line arguments.
+
+Arguments pairs are expressed in one of two forms:
+
+key=string
+key:=raw
+
+'key' and 'string' are unquoted JSON strings.
+'raw' is an encoded JSON value.
+
+Examples:
+
+$ exo json name=Alice
+{ "name": "Alice" }
+
+$ exo json status:=404 'message:="not found"'
+{ "status": 404, "message": "not found" }`,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		offline = true
+		return cmd.Parent().PersistentPostRunE(cmd, args)
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		obj, err := cmdutil.ArgsToJsonObject(args)
+		if err != nil {
+			return err
+		}
+		enc := json.NewEncoder(os.Stdout)
+		if jsonFlags.Indent {
+			enc.SetIndent("", "  ")
+		}
+		return enc.Encode(obj)
+	},
+}
