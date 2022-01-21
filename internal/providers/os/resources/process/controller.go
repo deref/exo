fc5b743bbcc08a25e488a9feaed75d93e3af003b
Brandon Bloom <brandon.d.bloom@gmail.com>
2022-01-20 16:15:00 -0800

    initial process controller

---
+++ internal/providers/os/resources/process/controller.go
@@ -0,0 +1,51 @@
+package process
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+
+	"github.com/deref/exo/internal/providers/sdk"
+	"github.com/deref/exo/internal/util/osutil"
+)
+
+type Controller struct{}
+
+func (c *Controller) Identify(ctx context.Context, m *Model) (string, error) {
+	// TODO: Validate host id.
+	if m.Pid == nil {
+		return "", nil
+	}
+	return fmt.Sprintf("exo:hosts/%s/processes/%d", m.HostID, *m.Pid), nil
+}
+
+func (c *Controller) Create(ctx context.Context, m *Model) error {
+	// TODO: Verify current host id.
+	cmd := &exec.Cmd{
+		Path: m.Program,
+		Args: append([]string{m.Program}, m.Arguments...),
+	}
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+	m.Pid = &cmd.Process.Pid
+	return nil
+}
+
+func (c *Controller) Read(ctx context.Context, m *Model) error {
+	if !c.Exists(ctx, m) {
+		return sdk.ErrResourceGone
+	}
+	return nil
+}
+
+func (c *Controller) Exists(ctx context.Context, m *Model) bool {
+	return m.Pid != nil && osutil.IsValidPid(*m.Pid)
+}
+
+func (c *Controller) Delete(ctx context.Context, m *Model) error {
+	if m.Pid == nil {
+		return nil
+	}
+	return osutil.KillGroup(*m.Pid)
+}
