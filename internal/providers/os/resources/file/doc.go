4b1eb9d4b2d496a274b4ec284819e59e1c6b86a9
Brandon Bloom <brandon.d.bloom@gmail.com>
2022-01-20 17:37:04 -0800

    untested file controller

---
+++ internal/providers/os/resources/file/doc.go
@@ -0,0 +1,4 @@
+// Package file provides a controller for typical operating system files.  This
+// is not super useful in practice, but it's very useful for testing the
+// controller system.
+package file
