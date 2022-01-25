e11806677d515d1c75d5a85cb131b36ff4a3f02f
Brandon Bloom <brandon.d.bloom@gmail.com>
2022-01-25 13:31:00 -0800

    reconcilation signatures

--- internal/resolvers/reconciliation.go
+++ internal/resolvers/reconciliation.go
@@ -1,6 +1,27 @@
 package resolvers
 
+import (
+	"context"
+	"errors"
+	"fmt"
+)
+
 type ReconciliationResolver struct {
 	Component *ComponentResolver
 	Job       *TaskResolver
+}
+
+func (r *MutationResolver) ReconcileComponent(ctx context.Context, args struct {
+	Stack *string
+	Ref   string
+}) (*VoidResolver, error) {
+	component, err := r.componentByRef(ctx, args.Ref, args.Stack)
+	if err != nil {
+		return nil, fmt.Errorf("resolving component: %w", err)
+	}
+	if component == nil {
+		return nil, errors.New("no such component")
+	}
+	// XXX reconcile here.
+	return nil, nil
 }
