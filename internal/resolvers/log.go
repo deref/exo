d3e394e6a1cea9bb7a2ee8b0975a05a291cda0ce
Brandon Bloom <brandon.d.bloom@gmail.com>
2022-02-09 18:27:06 -0800

    configure graphql resolver logging

---
+++ internal/resolvers/log.go
@@ -0,0 +1,26 @@
+package resolvers
+
+import (
+	"context"
+	"runtime"
+
+	"github.com/deref/exo/internal/util/logging"
+)
+
+type GraphqlLogger struct {
+	log logging.Logger
+}
+
+func NewGraphqlLogger(logger logging.Logger) *GraphqlLogger {
+	return &GraphqlLogger{
+		log: logger,
+	}
+}
+
+// Adapted from graphql-gopher's DefaultLogger implementation.
+func (gl *GraphqlLogger) LogPanic(ctx context.Context, value interface{}) {
+	const size = 64 << 10
+	buf := make([]byte, size)
+	buf = buf[:runtime.Stack(buf, false)]
+	gl.log.Infof("graphql: panic occurred: %v\n%s\ncontext: %v", value, buf, ctx)
+}
