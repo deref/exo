555dfa3c0604195fca2640ce07187b7ee6cbeb14
Brandon Bloom <brandon.d.bloom@gmail.com>
2022-03-08 13:50:38 -0800

    subscription for system version upgrades

--- internal/resolvers/system.go
+++ internal/resolvers/system.go
@@ -2,6 +2,7 @@ package resolvers
 
 import (
 	"context"
+	"time"
 )
 
 type SystemResolver struct {
@@ -24,4 +25,34 @@ func (r *SystemResolver) eventPrototype(ctx context.Context) (EventRow, error) {
 
 func (r *SystemResolver) Version() *VersionInfoResolver {
 	return resolveVersionInfo()
+}
+
+func (r *RootResolver) SystemChange(ctx context.Context) <-chan *SystemResolver {
+	c := make(chan *SystemResolver, 1)
+
+	var upgrade *string
+	{
+		system := r.System()
+		c <- system
+		upgrade = system.Version().Upgrade(ctx)
+	}
+
+	// Poll for available upgrades.
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(60 * time.Second):
+				system := r.System()
+				newUpgrade := system.Version().Upgrade(ctx)
+				if newUpgrade != nil && (upgrade == nil || *upgrade != *newUpgrade) {
+					upgrade = newUpgrade
+					c <- system
+				}
+			}
+		}
+	}()
+
+	return c
 }
