ff7c8123609c96dd359010ebfedc0fbe397f9f7c
Brandon Bloom <brandon.d.bloom@gmail.com>
2022-03-15 17:32:08 -0700

    extract Sparkline component

---
+++ gui/src/components/Sparkline.stories.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import Sparkline from './Sparkline.svelte';
+</script>
+
+<Meta title="Sparkline" component={Sparkline} />
+
+<Template let:args>
+  <Sparkline {...args} />
+</Template>
+
+<Story
+  name="Default"
+  args={{
+    entries: [0, 1, 2, 3, 4, 3, 2, 1, 2, 3, 4],
+    interactive: true,
+  }}
+/>
